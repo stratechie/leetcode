2a2d6780ae35b6fa60b71f545acbd32d3347eb4e
Tatev <tsrapivonyan@gmail.com>
2025-03-18 21:12:53 +0400

    1480

--- easy/problem_1480.cpp
+++ easy/problem_1480.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
-        vector<int> rSum;
-
-        rSum.push_back(nums[0]);
         for (size_t i = 1; i < nums.size(); ++i)
         {
-            rSum.push_back(rSum[i - 1] + nums[i]);
+            nums[i] += nums[i - 1];
         }
 
-        return rSum;
+        return nums;
     }
 };
