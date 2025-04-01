e74b9a199c546364da92469b73dd1b3a50924339
Tatev <tsrapivonyan@gmail.com>
2025-04-01 22:54:40 +0400

    383

---
+++ easy/problem_383.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        if (magazine.size() < ransomNote.size())
+        {
+            return false;
+        }
+
+        map<char, int> mapping;
+
+        for (auto& m : magazine)
+        {
+            mapping[m] += 1;
+        }
+
+        for (auto& r : ransomNote)
+        {
+            if (mapping[r] == 0)
+            {
+                return false;
+            }
+
+            mapping[r] -= 1;
+        }
+
+        return true;
+    }
+};
